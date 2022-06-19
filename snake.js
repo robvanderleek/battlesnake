111c97bcbfb599db26790bb1c30446eb5d616368
Rob van der Leek <robvanderleek@gmail.com>
2022-06-19 22:53:01 +0200

    First two challanges

--- snake.js
+++ snake.js
@@ -1,24 +1,76 @@
 #!/usr/bin/env node
 const express = require('express');
 const localtunnel = require('localtunnel');
+const packageJson = require('./package.json');
 
 const app = express();
 const port = 3000;
-const subdomain = 'tidy-cameras-guess-83-81-204-111';
+const subdomain = 'battlesnake-83-81-204-111';
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     console.log('/ called!');
-    res.send('hello world');
+    const response = {
+        'apiversion': '1',
+        'author': 'robvanderleek',
+        'color': '#C00000',
+        'head': 'caffeine',
+        'tail': 'curled',
+        'version': packageJson.version
+    }
+    res.send(response);
+});
+
+app.post('/start', (req, res) => {
+    const body = req.body;
+    console.log(`/start called for game: ${body.game.id}`);
+});
+
+const equal = (p1, p2) => p1.x === p2.x && p1.y === p2.y;
+
+const free = (snake, p) => !snake.body.some(bp => equal(p, bp));
+
+const right = (snake, board) => snake.head.x < board.width - 1 &&
+    free(snake, {x: snake.head.x + 1, y: snake.head.y});
+
+const down = (snake, board) => snake.head.y > 0 &&
+    free(snake, {x: snake.head.x, y: snake.head.y - 1});
+
+const left = (snake, board) => snake.head.x > 0 &&
+    free(snake, {x: snake.head.x - 1, y: snake.head.y});
+
+app.post('/move', (req, res) => {
+    const {turn, you: snake, board} = req.body;
+    console.log(`/move called for turn: ${turn}`); 
+    if (right(snake, board)) {
+        res.send({'move': 'right'});
+    } else if (down(snake, board)) {
+        res.send({'move': 'down'});
+    } else if (left(snake, board)) {
+        res.send({'move': 'left'});
+    } else {
+        res.send({'move': 'up'});
+    }
 });
 
 async function startTunnel() {
     const tunnel = await localtunnel({port: port, subdomain: subdomain});
     console.log(tunnel.url);
     tunnel.on('close', () => { console.log('tunnel closed') });
+    process.on("SIGINT", () => {
+      tunnel.close();
+      process.exit(0);
+    });
     return tunnel;
 }
 
 (async () => { 
     const tunnel = await startTunnel(); 
-    app.listen(port, () => { console.log(`App listening on port ${port}`); });
+    try {
+        app.listen(port, () => { console.log(`App listening on port ${port}`); });
+    } catch (e) {
+        tunnel.close();
+        console.error(e);
+    }
 })();
